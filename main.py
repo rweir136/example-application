9ab738636b6f6d2fc43a323940edd59310c5bd48
rweir136 <112025509+rweir136@users.noreply.github.com>
2026-07-09 14:12:21 -0400

    Update greeting message to include version 1.0

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import os
 from flask import Flask
-
 app = Flask(__name__)
-version = os.getenv("APP_VERSION", "unknown")
 
 @app.route('/')
 def index():
-    return f'Hello Argo CD {version}!'
+    return 'Hello Argo CD v1.0!'
 
 app.run(host='0.0.0.0', port=8080)
