a32f5f096b97c36c2c54aa1ad132d56d162b8a30
rweir136 <112025509+rweir136@users.noreply.github.com>
2026-07-09 10:04:11 -0400

    Dynamic versioning in greeting message

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+version = os.getenv("APP_VERSION", "unknown")
 
 @app.route('/')
 def index():
-    return 'Hello Argo CD v1.0!'
+    return f'Hello Argo CD {version}!'
 
 app.run(host='0.0.0.0', port=8080)
